4332da98f4711eb0d1a33d4fc302f99d9e1c2cd3
Alan Russell <ajrussellaudio@gmail.com>
2018-04-07 17:28:45 +0100

    add midi input connection at top level

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  onMidiMessage = message => {
+    console.log(message);
+  };
+
+  onMidiSuccess = midiAccess => {
+    for (let input of midiAccess.inputs.values()) {
+      input.onmidimessage = this.onMidiMessage;
+    }
+  };
+
+  componentDidMount() {
+    if (navigator.requestMIDIAccess) {
+      navigator
+        .requestMIDIAccess({
+          sysex: false
+        })
+        .then(this.onMidiSuccess, console.error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
